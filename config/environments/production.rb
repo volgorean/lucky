ffa549ec797edf58251ab8e23577fe3b7375667e
peter <peter@Natalies-iMac.fios-router.home>
2018-07-24 13:07:03 -0700

    changed name to lucky

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "pMedia_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "lucky_#{Rails.env}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
